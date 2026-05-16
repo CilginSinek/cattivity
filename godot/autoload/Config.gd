b804a1ee456b7e6e701020cff73f4659fe9c6018
suat kıvam <suatkivam@gmail.com>
2026-05-16 22:57:17 +0300

    feat: Login JWT cookie via JavaScriptBridge for web build

--- godot/autoload/Config.gd
+++ godot/autoload/Config.gd
@@ -1,8 +1,9 @@
 # res://autoload/Config.gd
 extends Node
 
-# Development
 const BASE_URL: String = "http://localhost:5000"
+const FORTYTWO_CLIENT_ID: String = "BURAYA_CLIENT_ID"
+var jwt_token: String = ""
 
-# Production için değiştir:
-# const BASE_URL: String = "https://api.senin-site.com"
+func get_auth_header() -> String:
+	return "Bearer " + jwt_token
--- godot/scenes/Login.gd
+++ godot/scenes/Login.gd
@@ -3,33 +3,18 @@ extends Control
 
 @onready var login_button: Button = $LoginButton
 
-var _http: HTTPRequest
-var _polling: bool = false
-
 func _ready() -> void:
 	login_button.pressed.connect(_on_login_pressed)
-	
-	_http = HTTPRequest.new()
-	add_child(_http)
-	_http.request_completed.connect(_on_poll_complete)
-	
-	# Zaten login misin kontrol et
-	_poll()
+	_check_token()
+
+func _check_token() -> void:
+	var token = JavaScriptBridge.eval("document.cookie.match(/GameToken=([^;]+)/)?.[1] || ''")
+	if token != "":
+		Config.jwt_token = token
+		GameStateManager.go_to_menu()
 
 func _on_login_pressed() -> void:
-	OS.shell_open(Config.BASE_URL + "/auth/42")
-	_polling = true
-	_start_polling()
-
-func _start_polling() -> void:
-	while _polling:
-		await get_tree().create_timer(2.0).timeout
-		_poll()
-
-func _poll() -> void:
-	_http.request(Config.BASE_URL + "/")
-
-func _on_poll_complete(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
-	if response_code == 200:
-		_polling = false
-		GameStateManager.go_to_menu()
+	var client_id = Config.FORTYTWO_CLIENT_ID
+	var redirect_uri = (Config.BASE_URL + "/auth/42/callback").uri_encode()
+	var url = "https://api.intra.42.fr/oauth/authorize?client_id=" + client_id + "&redirect_uri=" + redirect_uri + "&response_type=code&scope=public"
+	JavaScriptBridge.eval("window.location.href = '" + url + "'")
