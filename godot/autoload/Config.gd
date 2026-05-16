42f1031afa1fba826bdf1fe7b09eb0458b9c4ffb
suat kıvam <suatkivam@gmail.com>
2026-05-16 19:42:13 +0300

    feat: SongManager HTTP download, zip extract, Config base URL

---
+++ godot/autoload/Config.gd
@@ -0,0 +1,8 @@
+# res://autoload/Config.gd
+extends Node
+
+# Development
+const BASE_URL: String = "http://localhost:5000"
+
+# Production için değiştir:
+# const BASE_URL: String = "https://api.senin-site.com"
--- godot/autoload/SongManager.gd
+++ godot/autoload/SongManager.gd
@@ -8,24 +8,56 @@ signal song_resumed
 
 var song_time: float = 0.0
 var is_playing: bool = false
-var offset_ms: float = 0.0  # kullanıcı ayarlarından gelecek
+var offset_ms: float = 0.0
 
 @onready var _player: AudioStreamPlayer = AudioStreamPlayer.new()
+var _http: HTTPRequest
 
 func _ready() -> void:
 	add_child(_player)
 	_player.finished.connect(_on_song_finished)
+	
+	_http = HTTPRequest.new()
+	add_child(_http)
+	_http.request_completed.connect(_on_download_complete)
 
 func _process(delta: float) -> void:
 	if is_playing:
 		song_time += delta * 1000.0
 
-func load_song(stream: AudioStream) -> void:
+func load_and_play(package_url: String) -> void:
+	var full_url = Config.BASE_URL + package_url
+	_http.request(full_url)
+
+func _on_download_complete(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
+	if result != HTTPRequest.RESULT_SUCCESS:
+		push_error("SongManager: download failed")
+		return
+	
+	var temp_path = "user://temp_package.zip"
+	var file = FileAccess.open(temp_path, FileAccess.WRITE)
+	file.store_buffer(body)
+	file.close()
+	
+	var zip = ZIPReader.new()
+	var err = zip.open(temp_path)
+	if err != OK:
+		push_error("SongManager: zip open failed")
+		return
+	
+	var mp3_data = zip.read_file("song.mp3")
+	zip.close()
+	
+	var stream = AudioStreamMP3.new()
+	stream.data = mp3_data
 	_player.stream = stream
+	
+	play()
 
 func play() -> void:
 	_player.play()
 	is_playing = true
+	song_time = 0.0
 	emit_signal("song_started")
 
 func pause() -> void:
